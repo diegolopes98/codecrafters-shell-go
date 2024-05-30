9343ce0a13fee2cee7a932096f3246c57dfce172
Diego Lopes <diegopereiralopes98@gmail.com>
2024-05-30 10:51:23 -0300

    feat: add loop to evaluate various commands

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -8,20 +8,39 @@ import (
 )
 
 func main() {
+	reader := bufio.NewReader(os.Stdin)
+
+	repl(reader)
+
+	os.Exit(0)
+}
+
+func repl(reader *bufio.Reader) {
+	for {
+		input := read(reader)
+
+		cmd := eval(input)
+
+		if valid := validateCommand(cmd); !valid {
+			fmt.Fprintf(os.Stdout, "%s: command not found\n", cmd)
+		}
+	}
+}
+
+func read(reader *bufio.Reader) string {
 	fmt.Fprint(os.Stdout, "$ ")
 
-	input, err := bufio.NewReader(os.Stdin).ReadString('\n')
+	input, err := reader.ReadString('\n')
 	if err != nil {
 		fmt.Fprintf(os.Stdout, "Error reading from buffer %s", err)
 		os.Exit(-1)
 	}
 
-	cmd := strings.TrimRight(input, "\n")
-	if valid := validateCommand(cmd); !valid {
-		fmt.Fprintf(os.Stdout, "%s: command not found\n", cmd)
-	}
+	return input
+}
 
-	os.Exit(0)
+func eval(input string) string {
+	return strings.TrimRight(input, "\n") // TODO: add proper evaluation
 }
 
 func validateCommand(cmd string) bool {
